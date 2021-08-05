dfd5b99da718dc7e33bd02d655f8b253f99d4725
Erinxon <30541728+Erinxon@users.noreply.github.com>
2021-08-05 11:21:42 -0400

    Archivo Markdown modificado

--- Models/SorteoEspecial.cs
+++ Models/SorteoEspecial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ namespace ApiLoteria.Models
 {
     public class SorteoEspecial : SorteoBase
     {
+        [JsonProperty(Order = 3)]
         public List<SorteoEspecialResultado> Numeros { get; set; }
     }
 
